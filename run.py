92609a031b131db2ec388b34e2d12418aedc631d
scriptnkp <scriptnkp@gmail.com>
2026-04-29 15:20:45 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+Tawee@421272
+Sc.665544
+
+
+<a href="https://imgbox.com/rQUZ8OAA" target="_blank"><img src="https://images2.imgbox.com/b3/4f/rQUZ8OAA_o.png" alt="imgbox"/></a> 
+ให้ขนาดภาพมีความใกล้เคียงภาพ 2
+
+ส่วนตาราง ภาพที่ 3 ให้เหมือนภาพที่ 4 ข้อความเหมือนกันหมด
